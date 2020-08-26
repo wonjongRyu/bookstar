ce63d270c720807b748f4b83cd428dc36b1a8152
wonjongRyu <wonjong4773@gmail.com>
2020-08-26 17:06:45 +0900

    merge typeDefs and resolvers into scheme

--- src/server.js
+++ src/server.js
@@ -1,29 +1,19 @@
 // dotenv module reads .env files
-require("dotenv").config()
+require("dotenv").config();
 
-// import GraphQL server from graphql-yoga
-import { GraphQLServer } from "graphql-yoga"
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema"
 
 // set the PORT
 const PORT = process.env.PORT || 4000;
 
-// definite types of queries
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`;
-
-// Example of a resolver
-const resolvers = {
-  Query: {
-    hello: () => "Hi"
-  }
-};
-
 // creat server 
-const server = new GraphQLServer({ typeDefs, resolvers });
+// GraphQLServer needs props including typeDefs and resolvers as args 
+const server = new GraphQLServer({ schema });
 
+// Print Log. Express server is built in GraphQLServer
+server.express.use(logger("dev"));
 
 // start server
 server.start({ port: PORT }, () =>
