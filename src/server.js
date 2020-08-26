10d09f39c6cc1e7e716b229b90cd81d95fcfeb2b
wonjongRyu <wonjong4773@gmail.com>
2020-08-26 16:19:12 +0900

    init graphql server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,31 @@
-console.log("hello")
+// dotenv module reads .env files
+require("dotenv").config()
+
+// import GraphQL server from graphql-yoga
+import { GraphQLServer } from "graphql-yoga"
+
+// set the PORT
+const PORT = process.env.PORT || 4000;
+
+// definite types of queries
+const typeDefs = `
+    type Query{
+        hello: String!
+    }
+`;
+
+// Example of a resolver
+const resolvers = {
+  Query: {
+    hello: () => "Hi"
+  }
+};
+
+// creat server 
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+
+// start server
+server.start({ port: PORT }, () =>
+  console.log(`Server running on  http://localhost:${PORT}`)
+);
